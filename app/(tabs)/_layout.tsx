ba2aba9acd999e7a3da90b79532701c1028c3dcd
Andrei Belskii <mrandrewer@yandex.ru>
2024-11-11 21:25:25 +0300

    Add navigation images

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 import { Tabs } from 'expo-router';
+import { Image} from "react-native";
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Обо мне' }} />
-      <Tabs.Screen name="myExperience" options={{ title: 'Мой опыт' }} />
+      <Tabs.Screen 
+        name="index"
+        options={{ 
+          title: 'Обо мне',
+          tabBarIcon: ({size, focused, color}) => {
+            return (
+              <Image
+                style={{ width: size, height: size }}
+                source={ require("../../assets/images/profile.png") }
+              />
+            );
+          }
+        }} />
+      <Tabs.Screen
+        name="myExperience"
+        options={{ 
+          title: 'Мой опыт',
+          tabBarIcon: ({size, focused, color}) => {
+            return (
+              <Image
+                style={{ width: size, height: size }}
+                source={ require("../../assets/images/portfolio.png") }
+              />
+            );
+          }
+        }} />
     </Tabs>
   );
 }
